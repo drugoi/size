7489158f9ae4118d9a98cc4addb02db15d293fff
Nikita Bayev <drugoi.nik@gmail.com>
2017-04-24 12:09:08 +0600

    Added all size field && styles with prefixes

--- size.js
+++ size.js
@@ -1,8 +1,9 @@
 var size = function() {
 	var documentWidth = window.innerWidth;
 	var documentHeight = window.innerHeight;
-	document.getElementById('width').innerHTML = documentWidth + 'пк';
-	document.getElementById('height').innerHTML = documentHeight + 'пк';
+	document.querySelector('.size__width').innerHTML = documentWidth + 'пк';
+	document.querySelector('.size__height').innerHTML = documentHeight + 'пк';
+	document.querySelector('.size__all').innerHTML = documentWidth + 'x' + documentHeight;
 };
 
 window.onload = size;
